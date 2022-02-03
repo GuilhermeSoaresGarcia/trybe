a3515ec991b65bf740526adc3e12c8a37193a6d8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-02-03 16:03:01 -0300

    Exercícios do conteúdo de for/of, bloco 04, dia 02

---
+++ 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-02-array-e-loop-for/exercicios-do-conteudo-for-of/numeros-de-um-array.js
@@ -0,0 +1,5 @@
+let numeros = ['Um','Dois','Três','Quatro','Cinco'];
+
+for(let numero of numeros) {
+  console.log(numero);
+}
---
+++ 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-02-array-e-loop-for/exercicios-do-conteudo-for-of/soma.js
@@ -0,0 +1,6 @@
+let arrayOfNumbers = [10, 20, 30];
+
+for (let sum of arrayOfNumbers) {
+  sum += 1
+  console.log(sum);
+}
