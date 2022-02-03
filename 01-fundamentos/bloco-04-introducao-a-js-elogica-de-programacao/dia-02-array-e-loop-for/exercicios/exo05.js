e1f3f980f17da84994b935441b0315410116d699
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-02-03 20:56:27 -0300

    Exercícios de fixação, do 01 ao 05, bloco 04, dia 02

---
+++ 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-02-array-e-loop-for/exercicios/exo05.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for (i = 0; i < numbers.sort().length; i += 1) {
+  console.log(numbers[i]);
+
+}
+
+// console.log(numbers[numbers.length +1])
