06371a0257b458ad2c612e3ece414896d8acd96b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-02-03 23:44:26 -0300

    Exercícios de fixação, do 05 ao 07, bloco 04, dia 02

--- 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-02-array-e-loop-for/exercicios/exo07.js
+++ 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-02-array-e-loop-for/exercicios/exo07.js
@@ -1,10 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let smaller = 0;
+let smaller = numbers[0] + 1;
 
 for (i = 0; i < numbers.length; i += 1) {
-  if (numbers[i] > numbers[i + 1] && numbers[i] > smaller) {
+  if (numbers[i] < numbers[i + 1] && numbers[i] < smaller) {
     smaller = numbers[i]
-  } else if (numbers[i] < numbers[i + 1] && numbers[i + 1] > smaller) {
+  } else if (numbers[i] > numbers[i + 1] && numbers[i + 1] < smaller) {
     smaller = numbers[i + 1]
   }
 }
