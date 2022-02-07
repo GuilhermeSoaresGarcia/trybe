0daaf91cf30990af198f913d4878482c087e7591
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-02-07 20:21:57 -0300

    Exercicios de fixação, 01 ao 05, bloco 04, dia 04

---
+++ 01-fundamentos/bloco-04-introducao-a-js-elogica-de-programacao/dia-04-js-objetos-e-funcoes/exercicios-fixacao/ex02.js
@@ -0,0 +1,19 @@
+/*2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+{
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}
+*/
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+}
+
+//Fonte: https://appdividend.com/2020/07/27/javascript-how-to-add-property-to-object/
+info.recorrente = 'Sim';
+
+console.log(info);
