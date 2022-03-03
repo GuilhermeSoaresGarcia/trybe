7682721309b15573d674bc75c61aa71d19d5d759
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-03 16:26:10 -0300

    Exercícios de fixação, bloco 07, dia 01, exercícios 1 e 2

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-strings/ex02.js
@@ -0,0 +1,4 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+let correctedNumbers = oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${correctedNumbers} se encontram ordenados de forma crescente!`);
