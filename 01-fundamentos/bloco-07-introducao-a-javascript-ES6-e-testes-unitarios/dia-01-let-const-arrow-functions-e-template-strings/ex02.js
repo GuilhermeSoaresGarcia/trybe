02ccebd45462705df3d9eb1eccbd5206fc1566b9
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-04 00:01:31 -0300

    Brincando com 'bubble sort'

--- 01-fundamentos/bloco-07-introducao-a-javascript-ES6-e-testes-unitarios/dia-01-let-const-arrow-functions-e-template-strings/ex02.js
+++
@@ -1,4 +0,0 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-let correctedNumbers = oddsAndEvens.sort((a, b) => a - b);
-
-console.log(`Os números ${correctedNumbers} se encontram ordenados de forma crescente!`);
