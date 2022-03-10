b8455766abb54b9e04580beae7c373fa30bdbd1a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-10 15:02:08 -0300

    Exercícios do conteúdo, bloco 08, dia 02, exercício 'forEach'

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-JS-ES6/dia-02-HOF-forEach-find-some-every-sort/exercicios-conteudo/ex01-forEach.js
@@ -0,0 +1,18 @@
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email, indice) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+  console.log(`Índice no array: ${indice}`)
+};
+
+// Use o método forEach chamando a callback showEmailList para apresentar os emails 
+// Adicione seu código aqui
+
+emailListInData.forEach((elemento, index) => {
+  showEmailList(elemento, index);
+})
