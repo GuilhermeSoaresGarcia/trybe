67e53b82905e0ac7510dfe77f5a6a345760f3a7b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-03-21 16:35:32 -0300

    Exercícios do conteúdo, bloco 09, dia 01

---
+++ 01-fundamentos/bloco-09-js-e-testes-assincronos/dia-01-js-assincrono-e-callbacks/exercicios-conteudo/ex01-estoque-parafusos.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  console.log('0a - Comprar parafusos') // Comprar parafusos
+  console.log('0b - Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
