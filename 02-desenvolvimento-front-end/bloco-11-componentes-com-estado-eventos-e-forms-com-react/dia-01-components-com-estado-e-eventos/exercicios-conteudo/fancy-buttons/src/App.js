82653163ee0fb6d43d859dd576a346b327c708cc
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-04-08 14:31:50 -0300

    Exercícios do conteúdo, bloco 11, dia 01

---
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/dia-01-components-com-estado-e-eventos/exercicios-conteudo/fancy-buttons/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button buttonText="Texto 1" buttonColor="button-red" />
+      <Button buttonText="Texto 2" buttonColor="button-green" />
+      <Button buttonText="Texto 3" buttonColor="button-blue" />
+    </div>
+  );
+}
+
+export default App;
