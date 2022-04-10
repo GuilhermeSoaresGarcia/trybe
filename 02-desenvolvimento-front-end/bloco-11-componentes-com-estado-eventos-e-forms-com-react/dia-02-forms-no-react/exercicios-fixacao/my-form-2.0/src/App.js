662ed4740ea13450cfff8145661f3164a48ecba2
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-04-10 20:29:28 -0300

    Exercícios de fixação, bloco 11, dia 02

---
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/dia-02-forms-no-react/exercicios-fixacao/my-form-2.0/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PersonalData from './PersonalData';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalData/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02-desenvolvimento-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/dia-02-forms-no-react/exercicios-fixacao/my-form-2.0/src/PersonalData.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import estados from './data/estados';
+import './PersonalData.css';
+
+class PersonalData extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      nome: '',
+      email: '',
+      cpf: 0,
+      endereco: '',
+      cidade: '',
+      estado: '',
+      moradia: '',
+    }
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.name === 'nome' ? target.value.toUpperCase() : target.value,
+    })
+  }
+
+  render() {
+    return (
+      <fieldset>
+
+        <label htmlFor="name">
+          Nome: <input name="nome" id="name" autoComplete="off"
+            value={this.state.nome}
+            onChange={this.handleChange} />
+        </label>
+
+        <label htmlFor="email">
+          E-mail: <input type="email" name="email" id="email" 
+          value={this.state.email} 
+          onChange={this.handleChange} required />
+        </label>
+
+        <label htmlFor="cpf">
+          CPF: <input type="number" name="cpf" id="cpf" 
+          value={this.state.cpf} 
+          onChange={this.handleChange} />
+        </label>
+
+        <label htmlFor="endereco">
+          Endereço: <input name="endereco" id="address" 
+          value={this.state.endereco} 
+          onChange={this.handleChange} required />
+        </label>
+
+        <label htmlFor="cidade">
+          Cidade: <input name="cidade" id="city" 
+          value={this.state.cidade} 
+          onChange={this.handleChange} />
+        </label>
+
+        <label htmlFor="estado">
+          Estado: <select name="estado" id="state" 
+          onChange={this.handleChange}>
+            <option value="" required></option>
+            {estados.map((estado) => (
+              <option key={estado.sigla} value={estado.sigla}>
+                {estado.nomeEstado} - {estado.sigla}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="moradia" onChange={this.handleChange} required>
+          Tipo: <label htmlFor="casa">Casa
+            <input type="radio" name="moradia" id="casa" value="casa" />
+          </label>
+          <label htmlFor="ap">Apartamento
+            <input type="radio" name="moradia" id="ap" value="apartamento" />
+          </label>
+        </label>
+      </fieldset >
+    )
+  }
+}
+
+export default PersonalData;
