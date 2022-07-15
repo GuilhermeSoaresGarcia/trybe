daf5a45dfc969e9918d1704d18ff135f6dc66697
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-07-14 22:30:39 -0300

    Exercícios do conteúdo, bloco 20, dia 03

---
+++ 03-backend/bloco-20-intro-SQL/dia-03-filtrando-dados-de-forma-especifica/exercicios-conteudo/04-date-sakila.sql
@@ -0,0 +1,40 @@
+-- Quantos pagamentos temos com a data de retorno 2005-05-25? Há múltiplas maneiras possíveis de encontrar a resposta.
+
+SELECT COUNT(*) FROM rental WHERE DATE(return_date) = '2005-05-25';
+
+SELECT COUNT(*) FROM rental WHERE return_date LIKE '2005-05-25%';
+
+SELECT COUNT(*)
+FROM rental
+WHERE
+    return_date BETWEEN '2005-05-25 00:00:00' AND '2005-05-25 23:59:59';
+
+-- Quantos pagamentos foram feitos entre 01/07/2005 e 22/08/2005?
+
+SELECT COUNT(*)
+FROM payment
+WHERE
+    payment_date BETWEEN '2005-07-01 00:00:00' AND '2005-08-22 23:59:59';
+
+SELECT COUNT(*)
+FROM payment
+WHERE
+    DATE(payment_date) BETWEEN '2005-07-01' AND '2005-08-22';
+
+-- Usando a tabela rental, extraia data, ano, mês, dia, hora, minuto e segundo dos registros com rental_id = 10330. Utilize a coluna rental_date para extrair as informações.
+
+SELECT
+    DATE(rental_date) AS 'Data',
+    YEAR(rental_date) AS 'Ano',
+    MONTH(rental_date) AS 'Mês',
+    DAY(rental_date) AS 'Dia',
+    HOUR(rental_date) AS 'Hora',
+    MINUTE(rental_date) AS 'Minuto',
+    SECOND(rental_date) AS 'Segundo'
+FROM rental
+WHERE rental_id = 10330;
+
+-- Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
+SELECT * FROM payment 
+WHERE DATE(payment_date) = '2005-07-28'
+AND HOUR(payment_date) > 22;
