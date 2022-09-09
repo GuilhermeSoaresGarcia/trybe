69fc3630c2eb68ffd676fd9ba6543de2c995bdd7
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-09 15:29:36 -0300

    Exercícios bloco 26, dia 01

---
+++ 03-backend/bloco-26-typescript/dia-01-intro-typescript/exercicios-conteudo/dias_da_semana.ts
@@ -0,0 +1,34 @@
+enum diasDaSemana {
+  domingo,
+  segunda,
+  terca,
+  quarta,
+  quinta,
+  sexta
+}
+
+enum cores_do_arco_iris {
+  vermelho,
+  laranja,
+  amarelo,
+  verde,
+  azulclaro,
+  azulescuro
+}
+
+enum menu {
+  salvar = 1,
+  imprimir = 2,
+  abrir = 3,
+  visualizar = 4,
+  fechar = 5
+}
+
+enum pontos_cardeais {
+  norte = 'N',
+  sul = 'S',
+  leste = 'L',
+  oeste = 'O'
+}
+
+console.log(diasDaSemana.segunda)
