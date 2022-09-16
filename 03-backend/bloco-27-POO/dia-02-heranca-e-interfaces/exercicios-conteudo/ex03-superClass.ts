4620d4c80897c3a62d9c3f616676e6832a823499
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-16 19:28:12 -0300

    Exercicios do conteudo, bloco 27, dia 02

--- 03-backend/bloco-27-POO/dia-02-heranca-e-interfaces/exercicios-conteudo/ex03-superClass.ts
+++ 03-backend/bloco-27-POO/dia-02-heranca-e-interfaces/exercicios-conteudo/ex03-superClass.ts
@@ -2,10 +2,42 @@
 
 // 2 - Dentro da função que recebe um objeto da Superclass como parâmetro, cheque o valor do atributo isSuper e imprima no console “Super!” se for true e “Sub!” se for false;
 
+// class Superclass3 {
+//   constructor(protected _isSuper: boolean = true) { }
 
+//   public sayHello() {
+//     console.log('Olá mundo!')
+//   }
+
+//   public get isSuper() {
+//     return this._isSuper;
+//   }
+// }
+
+// class Subclass3 extends Superclass3 {
+//   constructor(_isSuper: boolean) {
+//     super(_isSuper);
+//   }
+
+//   public get isSuper() {
+//     return this._isSuper;
+//   }
+// }
+
+// const objSup3 = new Superclass3(true);
+// const objSub3 = new Subclass3(false);
+
+// function myFunc(obj: Superclass3) {
+//   obj.sayHello();
+//   if(obj.isSuper) console.log('Super!!');
+//   else console.log('Sub!!');
+// }
+
+// myFunc(objSup3);
+// myFunc(objSub3);
 
 class Superclass3 {
-  constructor(public isSuper: boolean = true) { }
+  constructor(readonly _isSuper: boolean = true) { }
 
   public sayHello() {
     console.log('Olá mundo!')
@@ -13,21 +45,18 @@ class Superclass3 {
 }
 
 class Subclass3 extends Superclass3 {
-  // constructor() {
-    // super(Superclass3 = false)
-  // }
+  constructor(_isSuper: boolean) {
+    super(_isSuper);
+  }
 }
 
-
 const objSup3 = new Superclass3();
-const objSub3 = new Subclass3();
-
+const objSub3 = new Subclass3(false);
 
 function myFunc(obj: Superclass3) {
   obj.sayHello();
-  if(obj.isSuper) console.log('Super!!');
+  if(obj._isSuper) console.log('Super!!');
   else console.log('Sub!!');
-
 }
 
 myFunc(objSup3);
