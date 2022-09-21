dcd87d46e9e3d3ea146a898ebb1b0ef0987e5371
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-09-21 14:09:59 -0300

    Exercícios de fixação, bloco 27, dia 01

---
+++ 03-backend/bloco-27-POO/dia-01-intro-POO/exercicios-fixacao/ex03-04.ts
@@ -0,0 +1,55 @@
+class Client {
+  constructor(private _name: string) { }
+
+  get name(): string {
+    return this._name;
+  }
+}
+
+class Item {
+  constructor(private _itemName: string, readonly price: number) { }
+
+  get item(): string {
+    return this._itemName;
+  }
+}
+
+class Order {
+  private _client: Client; // no binding da linha 24 isso não tá sendo usado?
+  private _orderItems: Array<Item>;
+  private _paymentMethod: string;
+  private _discountPercentage?: number;
+
+  constructor(
+    client: Client,
+    orderItems: Array<Item>,
+    paymentMethod: string,
+    discountPercentage?: number) {
+    this._client = client;
+    this._orderItems = orderItems;
+    this._paymentMethod = paymentMethod;
+    this._discountPercentage = discountPercentage;
+  }
+
+  total(): number {
+    return this._orderItems.reduce((acc, curr) => curr.price + acc, 0)
+  }
+
+  totalWithDiscount(): number {
+    return this.total() * (1 - (this._discountPercentage || 0))
+  }
+}
+
+const cliente = new Client('João');
+const item1 = new Item('Batata frita', 5);
+const item2 = new Item('Guaraná Jesus', 2);
+const order1 = new Order(cliente, [item1, item2], 'Crédito', .1);
+
+console.log(cliente.name)
+console.log(item1.item, item1.price)
+
+console.log(order1)
+
+console.log(order1.total());
+console.log(order1.totalWithDiscount());
+
---
+++ 03-backend/bloco-27-POO/dia-01-intro-POO/exercicios-fixacao/ex05.ts
@@ -0,0 +1,17 @@
+class Data {
+  constructor(private _day: number, private _month: number, private _year: number) { }
+
+  get day() {
+    return this._day;
+  }
+
+  get month() {
+    return this._month;
+  }
+
+  get year() {
+    return this._year;
+  }
+}
+
+
