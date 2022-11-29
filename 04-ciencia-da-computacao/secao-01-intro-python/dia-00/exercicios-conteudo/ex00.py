9732f9b3928bbf6908022730788e6227e22e7028
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-11-29 19:29:14 -0300

    Exercícios

---
+++ 04-ciencia-da-computacao/secao-01-intro-python/dia-00/exercicios-conteudo/ex00.py
@@ -0,0 +1,12 @@
+
+# Exercício 5: Adicione o elemento “Ciência da Computação” à lista.
+
+trybe_course = ['Introdução', 'Front-end', 'Back-end']
+
+trybe_course.append('Ciência da Computação')
+
+print(trybe_course)
+
+trybe_course[0] = 'Fundamentos'
+
+print(trybe_course)
---
+++ 04-ciencia-da-computacao/secao-01-intro-python/dia-00/exercicios-conteudo/ex01.py
@@ -0,0 +1,15 @@
+var = set()
+
+print(var)
+
+var.add('guilherme')
+
+print(var)
+
+teste = {''}
+
+print(teste)
+
+teste.add('guilherme')
+
+print(teste)
