59c77af6d65cbf7d97504b927bc290207dad5e1a
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-13 16:26:52 -0300

    feat: adicionadas annotations EntityScan, EnableJpaRepositories e ComponentScan do Spring na classe principal da aplicação

--- 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/AlexandriaApplication.java
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/AlexandriaApplication.java
@@ -2,8 +2,14 @@ package com.betrybe.alexandria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.betrybe.alexandria.models.entities") // Indica ao Spring Data JPA qual sub-pacote da aplicação que contém as entidades
+@EnableJpaRepositories("com.betrybe.alexandria.models.repositories") // Indica ao Spring Data JPA qual sub-pacote da aplicação contém os repositórios
+@ComponentScan("com.betrybe.alexandria") //  Indica ao Spring (não confundir com o Spring Data JPA) onde deve inicializar a busca por componentes (Controllers, Services, Beam, etc)
 public class AlexandriaApplication {
 
 	public static void main(String[] args) {
