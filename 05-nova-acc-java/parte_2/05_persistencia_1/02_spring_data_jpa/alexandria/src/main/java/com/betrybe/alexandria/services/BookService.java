2b53058d88b37ef576cdfb444c556791c8e2777b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-19 19:55:53 -0300

    feat: importado BookDetailsRepository na classe BookService e adicionados métodos relativos aos BookDetails

--- 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/services/BookService.java
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/services/BookService.java
@@ -1,11 +1,11 @@
 package com.betrybe.alexandria.services;
 
 import com.betrybe.alexandria.models.entities.Book;
+import com.betrybe.alexandria.models.entities.BookDetails;
+import com.betrybe.alexandria.models.repositories.BookDetailsRepository;
 import com.betrybe.alexandria.models.repositories.BookRepository;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +13,12 @@ import org.springframework.stereotype.Service;
 public class BookService {
 
   private BookRepository bookRepository;
+  private BookDetailsRepository bookDetailsRepository;
 
   @Autowired
-  public BookService(BookRepository bookRepository) {
+  public BookService(BookRepository bookRepository, BookDetailsRepository bookDetailsRepository) {
     this.bookRepository = bookRepository;
+    this.bookDetailsRepository = bookDetailsRepository;
   }
 
   public Book insertBook(Book book) {
@@ -53,6 +55,40 @@ public class BookService {
 
   public List<Book> getAllBooks() {
     return bookRepository.findAll();
+  }
+
+  public BookDetails insertBookDetails(BookDetails bookDetails) {
+    return bookDetailsRepository.save(bookDetails);
+  }
+
+  public Optional<BookDetails> updateBookDetails(Long id, BookDetails bookDetails) {
+    Optional<BookDetails> optionalBookDetails = bookDetailsRepository.findById(id);
+
+    if (optionalBookDetails.isPresent()) {
+      BookDetails bookDetailsFromDB = optionalBookDetails.get();
+      bookDetailsFromDB.setSummary(bookDetails.getSummary());
+      bookDetailsFromDB.setPageCount(bookDetails.getPageCount());
+      bookDetailsFromDB.setYear(bookDetails.getYear());
+      bookDetailsFromDB.setIsbn(bookDetails.getIsbn());
+
+      BookDetails updatedBookDetails = bookDetailsRepository.save(bookDetailsFromDB);
+      return Optional.of(updatedBookDetails);
+    }
+
+    return optionalBookDetails;
+  }
+
+  public Optional<BookDetails> removeBookDetailsById(Long id) {
+    Optional<BookDetails> bookDetailsOptional = bookDetailsRepository.findById(id);
+
+    if (bookDetailsOptional.isPresent()) {
+      bookDetailsRepository.deleteById(id);
+    }
+    return bookDetailsOptional;
+  }
+
+  public Optional<BookDetails> getBookDetailsById(Long id) {
+    return bookDetailsRepository.findById(id);
   }
 
 }
