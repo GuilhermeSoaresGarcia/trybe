6361502f1bfcdc5ebee3e17c6209794477487486
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-19 19:35:05 -0300

    feat: criada classe BookDetail responsável pela entidade da camada model

---
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/models/entities/BookDetail.java
@@ -0,0 +1,77 @@
+package com.betrybe.alexandria.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book_details")
+public class BookDetail {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  private String summary;
+  
+  @Column(name = "page_count")
+  private int pageCount;
+  
+  private String year;
+  private String isbn;
+
+  public BookDetail() {
+  }
+
+  public BookDetail(Long id, String summary, int pageCount, String year, String isbn) {
+    this.id = id;
+    this.summary = summary;
+    this.pageCount = pageCount;
+    this.year = year;
+    this.isbn = isbn;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getSummary() {
+    return summary;
+  }
+
+  public void setSummary(String summary) {
+    this.summary = summary;
+  }
+
+  public int getPageCount() {
+    return pageCount;
+  }
+
+  public void setPageCount(int pageCount) {
+    this.pageCount = pageCount;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public void setYear(String year) {
+    this.year = year;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public void setIsbn(String isbn) {
+    this.isbn = isbn;
+  }
+
+}
