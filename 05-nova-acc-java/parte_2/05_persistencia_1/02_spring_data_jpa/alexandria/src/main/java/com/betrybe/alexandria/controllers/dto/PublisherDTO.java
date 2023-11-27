ffebc88fd3b562ce2165a34a046ed779facbdc58
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-27 08:28:52 -0300

    feat: implementado o atributo books ao PublisherDTO

--- 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/PublisherDTO.java
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/PublisherDTO.java
@@ -1,9 +1,12 @@
 package com.betrybe.alexandria.controllers.dto;
 
+import java.util.List;
+
+import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.models.entities.Publisher;
 
-public record PublisherDTO(Long id, String name, String address) {
+public record PublisherDTO(Long id, String name, String address, List<Book> books) {
   public Publisher toPublisher() {
-    return new Publisher(id, name, address);
+    return new Publisher(id, name, address, books);
   }
 }
