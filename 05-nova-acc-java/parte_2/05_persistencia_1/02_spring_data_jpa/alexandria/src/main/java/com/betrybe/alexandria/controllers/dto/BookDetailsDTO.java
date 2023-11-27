cec8f92135ebedfe14f00dd1a19ff45f516f1174
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-27 06:14:17 -0300

    fix: formato do JSON com camelCase ao invés do underline

--- 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/BookDetailsDTO.java
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/BookDetailsDTO.java
@@ -2,12 +2,13 @@ package com.betrybe.alexandria.controllers.dto;
 
 import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.models.entities.BookDetails;
-import com.fasterxml.jackson.annotation.JsonProperty;
+// import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BookDetailsDTO(
     Long id,
     String summary,
-    @JsonProperty("page_count") Integer pageCount,
+    // @JsonProperty("page_count") Integer pageCount, <=== pode ser usado assim caso queira que o json tenha o mesmo formato da tabela no BD
+    Integer pageCount,
     String year,
     String isbn,
     Book book) {
