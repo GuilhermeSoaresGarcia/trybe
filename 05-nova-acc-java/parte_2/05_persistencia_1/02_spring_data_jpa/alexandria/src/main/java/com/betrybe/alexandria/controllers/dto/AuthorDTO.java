be1dd3de6306c0ba1e98e767f5efb2cdf21cbc0b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-11-27 11:44:12 -0300

    feat: implementado o atributo null a chamada do método para a criação de uma nova instância dentro do método toAuthor em AuthorDTO

--- 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/AuthorDTO.java
+++ 05-nova-acc-java/parte_2/05_persistencia_1/02_spring_data_jpa/alexandria/src/main/java/com/betrybe/alexandria/controllers/dto/AuthorDTO.java
@@ -5,7 +5,7 @@ import com.betrybe.alexandria.models.entities.Author;
 public record AuthorDTO(Long id, String name, String nationality) {
 
   public Author toAuthor() {
-    return new Author(id, name, nationality);
+    return new Author(id, name, nationality, null);
   }
 
 }
